84faf6de7d9c42db6777ba90267b7562ab30a962
bochelord <bochelord@gmail.com>
2023-07-04 17:31:13 +0200

    Added fonts and connected startup scene to intro scene to allow some movement on the Build

--- Assets/_SAEGAME/Scripts/StartupController.cs
+++ Assets/_SAEGAME/Scripts/StartupController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class StartupController : MonoBehaviour
 {
@@ -15,9 +16,22 @@ public class StartupController : MonoBehaviour
     {
         faderImage.DOFade(0, 1.5f).OnComplete(() => {
             graphyDebug.SetActive(true);
+            LoadIntro();
         });
 
     }
 
-    
+
+    private void LoadIntro()
+    {
+
+        //Fake delay
+        faderImage.DOFade(0, 1f).OnComplete(() => {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        });
+
+
+    }
+
+
 }
