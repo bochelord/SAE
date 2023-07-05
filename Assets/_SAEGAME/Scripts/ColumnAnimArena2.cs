cd848a08fb92748d9a1ad00908291ba72e3d10c0
Bochelord <bochelord@gmail.com>
2023-07-05 17:32:26 +0200

    Adjusted sequence on Intro_hassen to check how the animation works

--- Assets/_SAEGAME/Scripts/ColumnAnimArena2.cs
+++ Assets/_SAEGAME/Scripts/ColumnAnimArena2.cs
@@ -9,6 +9,7 @@ public class ColumnAnimArena2 : MonoBehaviour
 {
 
     public GameObject Column;
+    public GameObject Column2;
 
     private void Start()
     {
@@ -20,7 +21,9 @@ public class ColumnAnimArena2 : MonoBehaviour
         print("reached");
         await Task.Delay(1000);
 
-        Column.transform.DOMoveY(Column.transform.position.y + 30f, 4.5f).SetEase(Ease.InOutQuad);
+        Column.transform.DOMoveY(Column.transform.position.y + 30f, 4.5f).SetEase(Ease.InOutQuad).OnComplete(()=> {
+            Column2.transform.DOMoveY(Column2.transform.position.y + 30f, 3f).SetEase(Ease.InOutQuad);
+        });
     }
 
 }
