8ff5de4fa61e716088d20ad809fa0a3a01884840
Bochelord <bochelord@gmail.com>
2023-07-19 17:38:13 +0200

    Added squence to start and quit the game

--- Assets/_SAEGAME/Scripts/StartupController.cs
+++ Assets/_SAEGAME/Scripts/StartupController.cs
@@ -11,27 +11,43 @@ public class StartupController : MonoBehaviour
     public Image faderImage;
     public GameObject graphyDebug;
     public bool showGraphyDebugConsole;
+    public float faderDelay = 1.5f;
+    public Button buttonPlay;
+    public Button buttonQuit;
 
     // Start is called before the first frame update
     void Start()
     {
-        faderImage.DOFade(0, 1.5f).OnComplete(() => {
+        faderImage.DOFade(0, faderDelay).OnComplete(() => {
             if (showGraphyDebugConsole)
             {
                 graphyDebug.SetActive(true);
             }
             
+            //LoadIntro();
+        });
+
+
+        //subscribe to buttons
+        buttonPlay.onClick.AddListener(() => {
             LoadIntro();
         });
 
+        buttonQuit.onClick.AddListener(() => {
+            Application.Quit();
+        });
+
     }
+
+
+    
 
 
     private void LoadIntro()
     {
 
         //Fake delay
-        faderImage.DOFade(0, 1f).OnComplete(() => {
+        faderImage.DOFade(1, faderDelay/2).OnComplete(() => {
             SceneManager.LoadScene(2, LoadSceneMode.Single);
         });
 
